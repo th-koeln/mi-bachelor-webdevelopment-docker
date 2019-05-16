8f36a9df4f87878c043fd4b59746239ddb0ebc2e
cnoss <christian.noss@fh-koeln.de>
2019-05-16 12:02:04 +0200

    Abhängigkeiten reduziert

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 . config.cfg
 
-docker run -it --rm -p 1313:1313 -v ${repo_mount_path}:/usr/share/blog --name ${image} ${container}
+# docker run -it --rm -p 1313:1313 -v ${repo_mount_path}:/usr/share/blog --name ${image} ${container}
+echo $(pwd)/site
+docker run -it --rm -p 1313:1313 -v $(pwd)/site:/usr/share/blog --name ${image} ${container}

658da4d2dd5b7920cd05ceab464242fa2d993b8c
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-05-17 13:44:25 +0200

    fix windowsbug

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,12 @@
 . ./config.cfg
 
 # docker run -it --rm -p 1313:1313 -v ${repo_mount_path}:/usr/share/blog --name ${image} ${container}
-echo $(pwd)/site
-docker run -it --rm -p 1313:1313 -v "$(pwd)/site:/usr/share/blog" --name ${container} ${image}
+echo "$(pwd)/site"
+
+case "$(uname -s)" in
+	MINGW*)
+		winpty docker run -it --rm -p 1313:1313 -v "/$(pwd)/site:/usr/share/blog" --name ${container} ${image}
+	;;
+	*)
+		docker run -it --rm -p 1313:1313 -v "$(pwd)/site:/usr/share/blog" --name ${container} ${image}
+esac
